b75184fc2daef48df57bd2a76268b7c0322c11f3
Brian Ruiz <brianruiz123@yahoo.com>
2019-04-30 23:42:36 -0500

    Update bivariate_plotting.py

--- bivariate_plotting.py
+++ bivariate_plotting.py
@@ -1,4 +1,13 @@
 import pandas as pd
-reviews = pd.read_csv('winemag-data_first150k.csv')
+import matplotlib.pyplot as plt
+
+reviews = pd.read_csv('winemag-data_first150k.csv')  # storeed locally into project
 
 print(reviews.head())
+
+#  Scatter plot (lowly) down sampled
+plt.show(reviews[reviews['price'] < 100].sample(300).plot.scatter(x='price', y='points'))
+
+#  Hex plot
+plt.show(reviews[reviews['price'] < 100].plot.hexbin(x='price', y='points', gridsize = 15))
+
