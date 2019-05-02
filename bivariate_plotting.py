296f6b30edc3b08e2af59387b30aa8cbb51e27ce
Brian Ruiz <brianruiz123@yahoo.com>
2019-05-01 19:26:35 -0500

    Update bivariate_plotting.py

--- bivariate_plotting.py
+++ bivariate_plotting.py
@@ -1,8 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-reviews = pd.read_csv('winemag-data_first150k.csv')  # storeed locally into project
-
+reviews = pd.read_csv('winemag-data_first150k.csv')  # stored locally into project
 print(reviews.head())
 
 #  Scatter plot (lowly) down sampled
@@ -22,3 +21,18 @@ plt.show(wine_counts.plot.area())
 
 #  Bivariate line chart
 plt.show(wine_counts.plot.line())
+
+#  ----------------------------------------------------
+
+pokemon = pd.read_csv("pokemon.csv", index_col=0)
+
+print(pokemon.head())
+plt.show(pokemon.plot.scatter(x='Attack', y='Defense'))
+
+pokemon_stats_legendary = pokemon.groupby(['Legendary', 'Generation']).mean()[['Attack', 'Defense']]
+
+plt.show(pokemon.plot.hexbin(x='Attack', y="Defense", gridsize=15))
+plt.show(pokemon_stats_legendary.plot.hexbin(x='Attack', y='Defense', gridsize=15))
+plt.show(pokemon_stats_legendary.plot.bar(stacked=True))
+pokemon_stats_by_generation = pokemon.groupby('Generation').mean()[['HP', 'Attack', 'Defense', 'Sp. Atk', 'Sp. Def', 'Speed']]
+plt.show(pokemon_stats_by_generation.plot.line())
