026341d63105f4f3005ce662047ffbc737abecea
Brian Ruiz <brianruiz123@yahoo.com>
2019-07-23 16:29:56 -0500

    Update 6_multivariate_plotting.py

--- 6_multivariate_plotting.py
+++ 6_multivariate_plotting.py
@@ -1,9 +1,20 @@
-import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from sklearn import tree
-from sklearn.ensemble import RandomForestRegressor
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error, r2_score
-from sklearn.model_selection import train_test_split
+
+pd.set_option('max_columns', None)
+df = pd.read_csv("Fifa18Dataset.csv", index_col=0)
+
+import re
+import numpy as np
+
+footballers = df.copy()
+footballers['Unit'] = df['Value'].str[-1]
+footballers['Value (M)'] = np.where(footballers['Unit'] == '0', 0,
+                                    footballers['Value'].str[1:-1].replace(r'[a-zA-Z]',''))
+footballers['Value (M)'] = footballers['Value (M)'].astype(float)
+footballers['Value (M)'] = np.where(footballers['Unit'] == 'M',
+                                    footballers['Value (M)'],
+                                    footballers['Value (M)']/1000)
+footballers = footballers.assign(Value=footballers['Value (M)'],
+                                 Position=footballers['Preferred Positions'].str.split().str[0])
+
+print(footballers.head())
