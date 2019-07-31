b11c0ac9de584de4ce17dc9ab6654cfebbb4b8f1
Brian Ruiz <brianruiz123@yahoo.com>
2019-07-31 00:42:11 -0500

    Update 6_multivariate_plotting.py

--- 6_multivariate_plotting.py
+++ 6_multivariate_plotting.py
@@ -25,13 +25,40 @@ print(footballers.head())
 
 import seaborn as sns
 import matplotlib.pyplot as plt
-
+'''
+# comparing value / wage to overall rating of 3 different player positions [st, cm, cb]
+# using visual variable of 'color' to distinguish differences
 plt.show(sns.lmplot(x='Value', y='Overall', hue ='Position',
            data = footballers.loc[footballers['Position'].isin(['ST', 'CM', 'CB'])],
            fit_reg= False))
 
-# Another example of a visual variable is 'shape', which controls marker shapes to help differentiate
+# another visual element for the plots include modifying the markers 'shape'
+# though, typically color is a better differentiator than shape
 sns.lmplot(x='Value', y='Overall', markers=['o', 'x', '*'], hue='Position',
            data=footballers.loc[footballers['Position'].isin(['ST', 'RW', 'LW'])],
            fit_reg=False
           )
+'''
+# A good comparison demonstrative plot is the 'grouped box plot'
+# I.e. we want to explore to find what position players score higher in 'aggression' attribute
+
+f = (footballers.loc[footballers['Position'].isin(['ST', 'CB'])]
+                .loc[:, ['Value', 'Overall', 'Aggression', 'Position']])
+
+f = f[f["Overall"] >= 80]
+f = f[f["Overall"] < 85]
+f['Aggression'] = f['Aggression'].astype(float)
+
+plt.show(sns.boxplot(x="Overall", y="Aggression", hue='Position', data=f))
+
+# We see how useful a box plot can be, however when we want to compare even more dimensions of data
+# we affect the interpretability of the plots by making them busier.
+# A solution to this would be summarization plots, an example of one is a 'heatmap' / 'correlation plot'
+
+f = (footballers.loc[:, ['Acceleration', 'Aggression', 'Agility', 'Balance', 'Ball control']]
+                .applymap(lambda v: int(v) if str.isdecimal(v) else np.nan)
+                .dropna()).corr()
+
+plt.show(sns.heatmap(f, annot = True))
+# Here we can now see correlations between different attributes and their
+
